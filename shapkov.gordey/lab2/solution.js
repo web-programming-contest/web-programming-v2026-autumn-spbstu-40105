76289c77e7043638fac2f0b76866aaded4bd6cc9
TheEmfield <emfield337@gmail.com>
2026-09-07 12:09:46 +0300

    format fix

--- shapkov.gordey/lab2/solution.js
+++ shapkov.gordey/lab2/solution.js
@@ -1,19 +1,19 @@
 export function analyzieString(str) {
-    let letters = 0;
-    let digits = 0;
-    let spaces = 0;
-    let other = 0;
+  let letters = 0;
+  let digits = 0;
+  let spaces = 0;
+  let other = 0;
 
-    for (const char of str) {
-        if (char === ' ') {
-            spaces++;
-        } else if (char >= '0' && char <= '9') {
-            digits++;
-        } else if (/^\p{L}$/u.test(char)) {
-            letters++;
-        } else {
-            other++;
-        }
+  for (const char of str) {
+    if (char === ' ') {
+      spaces++;
+    } else if (char >= '0' && char <= '9') {
+      digits++;
+    } else if (/^\p{L}$/u.test(char)) {
+      letters++;
+    } else {
+      other++;
     }
-    return { letters, digits, spaces, other };
+  }
+  return {letters, digits, spaces, other};
 }
