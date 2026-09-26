baa57a2908f2ae32b217deaf157112e50b86a1ae
dualya <duhaninaalina3@gmail.com>
2026-09-26 19:41:23 +0300

    prettier solution

--- duhanina.alina/lab2/solution.js
+++ duhanina.alina/lab2/solution.js
@@ -1,22 +1,23 @@
 export function isBalanced(str) {
-    const pairBrackets = {
-        "(": ")",
-        "{": "}",
-        "[": "]"
-    }
-    const stack = [];
+  const pairBrackets = {
+    '(': ')',
+    '{': '}',
+    '[': ']',
+  };
+  const stack = [];
 
-    for (let index = 0; index < str.length; index++) {
-        if (str[index] in pairBrackets) {
-            stack.push(str[index]);
-        }
-        else if (stack.length === 0 || pairBrackets[stack[stack.length - 1]] !== str[index]) {
-            return false;
-        }
-        else {
-            stack.pop();
-        }
+  for (let index = 0; index < str.length; index++) {
+    if (str[index] in pairBrackets) {
+      stack.push(str[index]);
+    } else if (
+      stack.length === 0 ||
+      pairBrackets[stack[stack.length - 1]] !== str[index]
+    ) {
+      return false;
+    } else {
+      stack.pop();
     }
+  }
 
-    return stack.length === 0;
+  return stack.length === 0;
 }
